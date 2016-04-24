a59e10429ee8d54452ac4fba307e32091c6ccfa2
Łukasz Makuch <kontakt@lukaszmakuch.pl>
2016-04-24 13:59:32 +0200

    scalar renderer interfaces

---
+++ src/ScalarRenderer/Exception/UnableToGetScalarRepresentation.php
@@ -0,0 +1,19 @@
+<?php
+
+/**
+ * This file is part of the TableRenderer library.
+ *
+ * @author Łukasz Makuch <kontakt@lukaszmakuch.pl>
+ * @license MIT http://opensource.org/licenses/MIT
+ */
+
+namespace lukaszmakuch\TableRenderer\ScalarRenderer\Exception;
+
+/**
+ * Thrown when it's impossible to get a scalar representation of some table model.
+ * 
+ * @author Łukasz Makuch <kontakt@lukaszmakuch.pl>
+ */
+class UnableToGetScalarRepresentation extends \RuntimeException
+{
+}
---
+++ src/ScalarRenderer/ScalarRenderer.php
@@ -0,0 +1,27 @@
+<?php
+
+/**
+ * This file is part of the TableRenderer library.
+ *
+ * @author Łukasz Makuch <kontakt@lukaszmakuch.pl>
+ * @license MIT http://opensource.org/licenses/MIT
+ */
+
+namespace lukaszmakuch\TableRenderer\ScalarRenderer;
+
+use lukaszmakuch\TableRenderer\ScalarRenderer\Exception\UnableToGetScalarRepresentation;
+use lukaszmakuch\TableRenderer\TableElement;
+
+/**
+ * Renders a model of a table as a scalar value or an array of scalar values.
+ * 
+ * @author Łukasz Makuch <kontakt@lukaszmakuch.pl>
+ */
+class ScalarRenderer
+{
+    /**
+     * @return mixed a scalar value or an array of scalar values (or arrays of them)
+     * @throws UnableToGetScalarRepresentation
+     */
+    public function getScalarRepresentationOf(TableElement $table);
+}
